ea3d679e8ce8aa9c08a84908be9ad3da2a8906de
Abbas Gure <abbas.gure@informationtechconsultants.co.uk>
2026-03-06 10:35:43 +0000

    app tsx modified

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,14 +4,10 @@ function App() {
 
     return (
         <>
-            <div>
-                <h1>Root</h1>
-                <h2>Main.tsx gets the root element and renders the root to contain an App component</h2>
-                <h3>The app file that is rendered</h3>
-            </div>
-            <div>
-                <RandomComponent/>
-            </div>
+            <h1>Root</h1>
+            <h2>Main.tsx gets the root element and renders the root to contain an App component</h2>
+            <h3>The app file that is rendered</h3>
+            <RandomComponent/>
         </>
     )
 }
