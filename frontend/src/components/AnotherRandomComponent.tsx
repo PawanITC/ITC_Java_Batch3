e75d91b53bad1e024a0b4e9b6376d31fa88a9bb0
Abbas Gure <abbas.gure@informationtechconsultants.co.uk>
2026-03-06 09:53:44 +0000

    added frontend and compose file to be tracked

---
+++ frontend/src/components/AnotherRandomComponent.tsx
@@ -0,0 +1,9 @@
+const AnotherRandomComponent = () => {
+    return (
+        <div>
+            <h1>Another random</h1>
+        </div>
+    );
+};
+
+export default AnotherRandomComponent;
---
+++ frontend/src/components/randomComponent.tsx
@@ -0,0 +1,12 @@
+import AnotherRandomComponent from "./AnotherRandomComponent.tsx";
+
+const RandomComponent = () => {
+    return (
+        <div>
+            <h1>Hi this is a random component</h1>
+            <AnotherRandomComponent />
+        </div>
+    );
+};
+
+export default RandomComponent;
