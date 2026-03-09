12aadb4a0c35f6a7afd6d48e9b48a38d61cd56cb
Abubakar <80838394+bakar212@users.noreply.github.com>
2026-03-09 17:56:06 +0000

    further frontend code

--- frontend/src/components/LoginJWT.tsx
+++ frontend/src/components/LoginJWT.tsx
@@ -1,7 +1,27 @@
 import './loginJWT.css'
 import { Link } from "react-router-dom";
-//onClick={handleGithubLogin}
+import { useState } from "react";
+
+
+
 function LoginJWT() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSubmit(e: { preventDefault: () => void; }) {
+        e.preventDefault();
+        console.log("Logging in with:", email, password);
+        // here you'd normally call your backend login API
+        window.alert("Logged in successfully!");
+    }
+
+    function handleGithubLogin() {
+        // Redirect to backend route that starts GitHub OAuth
+        window.location.href = "http://localhost:5000/auth/github";
+        // replace with your backend GitHub OAuth endpoint
+    }
+
     return (
         <div className="login-page">
 
@@ -9,13 +29,15 @@ function LoginJWT() {
 
                 <h1 className="login-title">Sign In</h1>
 
-                <form>
+                <form onSubmit={handleSubmit}>
 
                     <label>Email</label>
-                    <input type="email" placeholder="Enter your email" />
+                    <input type="email" placeholder="Enter your email" value={email}
+                           onChange={(e) => setEmail(e.target.value)}/>
 
                     <label>Password</label>
-                    <input type="password" placeholder="Enter your password" />
+                    <input type="password" placeholder="Enter your password"  value={password}
+                           onChange={(e) => setPassword(e.target.value)}/>
 
                     <button className="login-button">Sign In</button>
 
@@ -23,7 +45,7 @@ function LoginJWT() {
 
                 <div className="divider">OR</div>
 
-                <button  className="github-button">
+                <button  className="github-button" onClick={handleGithubLogin}>
                     <img
                         src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
                         alt="GitHub Logo"
