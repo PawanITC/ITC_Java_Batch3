530099e0a08f0e2f526bbc50d9f798abf7d2b6b8
Abbas Gure <abbas.gure@informationtechconsultants.co.uk>
2026-03-10 18:52:40 +0000

    merged css of abu with my work. removed unused env file.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,10 @@
-import {APP_NAME} from "../config/env.ts";
-
 export default function Home() {
 
     return (
 
         <div style={{ padding: "40px", textAlign: "center" }}>
 
-            <h1>Welcome to {APP_NAME}</h1>
+            <h1>Welcome to Funkart!!</h1>
 
             <p>
                 Discover and share creative artwork.
