1e4abfafcd2eb68394e058dc364bc081fadbae63
dipenitc <Dipendra.Hamal@informationtechconsultants.co.uk>
2026-03-12 22:13:37 +0000

    feat: implement products api end points

--- backend/product-service/src/main/java/com/itc/funkart/product_service/controller/ProductController.java
+++ backend/product-service/src/main/java/com/itc/funkart/product_service/controller/ProductController.java
@@ -1,4 +1,67 @@
 package com.itc.funkart.product_service.controller;
 
+import com.itc.funkart.product_service.dto.request.ProductCreateRequest;
+import com.itc.funkart.product_service.dto.request.ProductUpdateRequest;
+import com.itc.funkart.product_service.dto.response.ProductResponse;
+import com.itc.funkart.product_service.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(
+            @RequestBody ProductCreateRequest request) {
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(productService.createProduct(request));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProduct(
+            @PathVariable Long id) {
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(productService.getProduct(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAllProducts() {
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(productService.getAllProducts());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponse> updateProduct(
+            @PathVariable Long id,
+            @RequestBody ProductUpdateRequest request) {
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(productService.updateProduct(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(
+            @PathVariable Long id) {
+
+        productService.deleteProduct(id);
+
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build();
+    }
 }
