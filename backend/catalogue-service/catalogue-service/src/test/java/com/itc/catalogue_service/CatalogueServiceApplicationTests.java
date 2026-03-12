8066d4f4ecea1c846fa091aaa56371b04c1a02e2
PriyanthanSeevaratnam <Priyanthan.Seevaratnam@informationtechconsultants.co.uk>
2026-03-12 12:19:45 +0000

    setup catalogue-service microservice

---
+++ backend/catalogue-service/catalogue-service/src/test/java/com/itc/catalogue_service/CatalogueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itc.catalogue_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
