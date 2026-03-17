24e9bf3639840aebd47ca4156696faa5d4375d44
Abubakar <80838394+bakar212@users.noreply.github.com>
2026-03-17 14:02:50 +0000

    -added dependency for validation -implemented input validation for api request -modified controller endpoint to return response entity of type <ApiResponse>

--- backend/notification-service/src/main/java/com/example/notificationservice/controller/NotificationController.java
+++ backend/notification-service/src/main/java/com/example/notificationservice/controller/NotificationController.java
@@ -1,8 +1,12 @@
 package com.example.notificationservice.controller;
 
 import com.example.notificationservice.dto.OrderEventDTO;
+import com.example.notificationservice.response.ApiResponse;
 import com.example.notificationservice.service.NotificationService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 
 @RestController
@@ -10,14 +14,16 @@ import org.springframework.web.bind.annotation.*;
 public class NotificationController {
     private final NotificationService notificationService;
 
-    public NotificationController(NotificationService notificationService) {
+    public NotificationController(NotificationService notificationService) {//dependency injection for spring
         this.notificationService = notificationService;
     }
 
+
     @PostMapping("/order-event")
-    public void receiveOrderEvent(@RequestBody OrderEventDTO event) {
+    public ResponseEntity<ApiResponse<Void>> receiveOrderEvent(@RequestBody @Valid/*input validation*/ OrderEventDTO event) {
 
         notificationService.processOrderEvent(event);
-
+        return ResponseEntity.ok(new ApiResponse<>(null,"order event processed successfully"));
     }
 }
+//
--- backend/notification-service/src/main/java/com/example/notificationservice/dto/OrderEventDTO.java
+++ backend/notification-service/src/main/java/com/example/notificationservice/dto/OrderEventDTO.java
@@ -2,6 +2,9 @@ package com.example.notificationservice.dto;
 
 
 import com.example.notificationservice.event.OrderStatus;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,10 +12,13 @@ import lombok.Setter;
 @Setter
 @Getter
 public class OrderEventDTO {
-
+        @NotBlank(message = "There needs to be an order ID!") //input validation
         private String orderId;
+        @NotBlank(message = "There needs to be an e-mail at least!")
+        @Email(message = "This should be in a valid e-mail format!")
         private String email;
         private String phone;
+        @NotNull
         private OrderStatus status;
 
 }
