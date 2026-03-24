53cbc5fa7eb16e5fdf6bb2afbb6f52dd5a0265c2
Abubakar <80838394+bakar212@users.noreply.github.com>
2026-03-24 20:23:22 +0000

    -implmented a few unit tests for messagetemplatebuilder and mockEmail/mockSms sender

--- backend/notification-service/src/test/java/com/example/notificationservice/NotificationServiceApplicationTests.java
+++ backend/notification-service/src/test/java/com/example/notificationservice/NotificationServiceApplicationTests.java
@@ -1,12 +1,25 @@
 package com.example.notificationservice;
 
 import com.example.notificationservice.dto.OrderEventDTO;
+import org.hibernate.validator.internal.constraintvalidators.bv.AssertTrueValidator;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Assertions;
 
+import com.example.notificationservice.template.*;
+import com.example.notificationservice.event.OrderStatus;
+import com.example.notificationservice.service.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 @SpringBootTest
 class NotificationServiceApplicationTests {
+
+    @Autowired
+    private AssertTrueValidator assertTrueValidator;
 
     @Test
     void contextLoads() {
@@ -49,18 +62,45 @@ class NotificationServiceApplicationTests {
 
     @Test
     void validateMessageBuilderTemplateGenerateMessage() {
+
+        String message = MessageBuilderTemplate.generateMessage("123", OrderStatus.ORDER_CONFIRMED);
+
+        Assertions.assertEquals("Good news! Your order #123 has been confirmed.", message);
     }
 
     @Test
     void validateMessageBuilderTemplateGenerateSubject() {
+        String message = MessageBuilderTemplate.generateSubject("789989", OrderStatus.DELIVERED);
+
+        Assertions.assertEquals("789989 Status: Order delivered!", message);
     }
 
     @Test //the mock email sender should print out message to console when event parameter fields are valid
     void validateMockEmailSenderWhenEmailFieldIsNotNull() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = System.out;
+
+        System.setOut(new PrintStream(baos));//start new stream boundary
+
+        MockEmailSender mockEmailSender = new MockEmailSender();
+        mockEmailSender.sendEmail("holymoly@gmail.com","your order update", "your order is cancelled!");
+
+        System.setOut(ps);//reset new boundary
+
+        String output = baos.toString();
+
+        Assertions.assertTrue(output.contains("Sending email to: " + "holymoly@gmail.com"));
+        Assertions.assertTrue(output.contains("Email Subject: " + "your order update"));
+        Assertions.assertTrue(output.contains("Message: " + "your order is cancelled!"));
+
+
     }
 
     @Test //the mock email sender should not print anything to console
     void validateMockEmailSenderWhenEmailFieldIsEmpty() {
+
+
+
     }
 
     @Test //validate that the simple mail transfer protocol is able to send live emails when everything is correct such as authentication etc.
@@ -74,6 +114,21 @@ class NotificationServiceApplicationTests {
 
     @Test //the mock sms sender should print out message to console when event parameter fields are valid
     void validateMockSmsSenderWhenNumberIsNotNull() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = System.out;
+
+        System.setOut(new PrintStream(baos));//start new stream boundary
+
+        MockSmsSender mockSmsSender = new MockSmsSender();
+        mockSmsSender.sendSms("07890903948", "your order is cancelled!");
+
+        System.setOut(ps);//reset new boundary
+
+        String output = baos.toString();
+
+        Assertions.assertTrue(output.contains("Sending SMS to: " + "07890903948"));
+        Assertions.assertTrue(output.contains("Message: " + "your order is cancelled!"));
+
     }
 
     @Test //the mock sms sender should not print anything to console
