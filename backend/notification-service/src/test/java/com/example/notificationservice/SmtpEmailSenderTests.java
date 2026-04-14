5e10b1d55a34c446ad93327a774e3e4f6fd4da43
Abubakar <80838394+bakar212@users.noreply.github.com>
2026-04-14 20:39:07 +0100

    -added more test cases

--- backend/notification-service/src/test/java/com/example/notificationservice/SmtpEmailSenderTests.java
+++ backend/notification-service/src/test/java/com/example/notificationservice/SmtpEmailSenderTests.java
@@ -1,4 +1,49 @@
 package com.example.notificationservice;
 
+import com.example.notificationservice.exception.FailedToSendEmailException;
+import com.example.notificationservice.service.SmtpEmailSender;
+import jakarta.mail.internet.MimeMessage;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.MailSendException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest(properties = {
+        "management.health.mail.enabled=false"
+})//we need this config set to false otherwise mail health does not allow mocked javamailsender
 public class SmtpEmailSenderTests {
+    @Autowired
+    private SmtpEmailSender smtpEmailSender;
+    @MockBean
+    private JavaMailSender javaMailSender;
+
+    @Test
+    public void sendEmailTest(){
+
+        smtpEmailSender.sendEmail("idontknow@gmail.com","holiday","hey wassup hows the holiday");
+        Mockito.verify(javaMailSender).send(Mockito.any(SimpleMailMessage.class));
+    }
+
+    @Test
+    void sendEmail_shouldThrowCustomException_whenMailFails() {
+
+        // Arrange: force mailSender to throw exception
+        Mockito.doThrow(new MailSendException("SMTP error"))
+                .when(javaMailSender)
+                .send(Mockito.any(SimpleMailMessage.class));
+
+        // Act + Assert
+       assertThrows(FailedToSendEmailException.class, () -> {
+            smtpEmailSender.sendEmail("test@gmail.com", "subject", "message");
+       });
+    }
+
 }
