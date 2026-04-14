3bd7cd0c79da85cd82a02d0c1c2575b40b305aac
Abubakar <80838394+bakar212@users.noreply.github.com>
2026-04-14 16:29:29 +0100

    -refactoring code package structure

--- backend/notification-service/src/test/java/com/example/notificationservice/NotificationServiceImplTests.java
+++ backend/notification-service/src/test/java/com/example/notificationservice/NotificationServiceImplTests.java
@@ -24,7 +24,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 //@ExtendWith(MockitoExtension.class)
-class NotificationServiceServiceLayerTests {
+class NotificationServiceImplTests {
 
 
     @MockBean
@@ -217,37 +217,6 @@ class NotificationServiceServiceLayerTests {
         //was truly not called
 
     }
-
-    //------------------------FURTHER PERFORMANCE/OTHER TESTS--------------------------------------------------------------------------------------------------
-
-    @Test //testing resilliance4j's retry for requests sent to SMTP server
-    void retryTestForSmptpEmailSender() {
-    }
-
-    @Test //testing resilliance4j's retry for requests sent to TWILIO server
-    void retryTestForTwilioSmsSender() {
-    }
-
-    @Test //testing resilliance4j's circuitBreaker for requests sent to TWILIO server
-    void circuitBreakerTestForTwilioSmsSender() {
-    }
-
-    @Test //testing resilliance4j's timeLimiter for requests sent to TWILIO server
-    void TimeLimiterTestForTwilioSmsSender() {
-    }
-
-    @Test //testing fallback method for twilio sms sender activates when resilience4j metrics fail
-    void fallbackForTwilioSmsSender() {
-    }
-
-    @Test //testing fallback method for SMTP Email sender activates when resilience4j metrics fail
-    void fallbackForSmtpEmailSender() {
-    }
-
-    @Test //test whether the database connects with our credentials stored in secrets
-    void testDatabaseConnectivity() {
-    }
-
 
 
 }
