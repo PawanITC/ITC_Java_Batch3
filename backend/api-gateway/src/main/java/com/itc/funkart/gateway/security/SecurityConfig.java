0e5bc797f491047ab8b2f0ab9e0ca1c2942f066c
Abbas Gure <abbas.gure@informationtechconsultants.co.uk>
2026-04-16 10:53:00 +0100

    added entrypoint authentication to security config filter

--- backend/api-gateway/src/main/java/com/itc/funkart/gateway/security/SecurityConfig.java
+++ backend/api-gateway/src/main/java/com/itc/funkart/gateway/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.itc.funkart.gateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -34,6 +35,12 @@ public class SecurityConfig {
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
                 .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                 .exceptionHandling(ex -> ex
+                         .authenticationEntryPoint((exchange, ex2) -> {
+                             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                             return exchange.getResponse().setComplete();
+                         })
+                 )
                 .addFilterBefore(jwtWebFilter, SecurityWebFiltersOrder.AUTHENTICATION)
                 .authorizeExchange(auth -> auth
                         .pathMatchers(
