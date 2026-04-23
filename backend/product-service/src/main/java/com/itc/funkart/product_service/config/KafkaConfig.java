6e28bd158532e1065abd4509c7c5814b008b99d8
dipenitc <Dipendra.Hamal@informationtechconsultants.co.uk>
2026-04-23 22:57:16 +0100

    fix: resolve kafka connection issue with docker compose

--- backend/product-service/src/main/java/com/itc/funkart/product_service/config/KafkaConfig.java
+++ backend/product-service/src/main/java/com/itc/funkart/product_service/config/KafkaConfig.java
@@ -1,22 +1,11 @@
 package com.itc.funkart.product_service.config;
 
-import com.itc.funkart.product_service.dto.events.ProductEvent;
+import com.itc.funkart.product_service.constants.KafkaConstants;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.*;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.support.serializer.JsonSerializer;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @EnableKafka
@@ -24,7 +13,14 @@ public class KafkaConfig {
 
     @Bean
     public NewTopic productTopic() {
-        return TopicBuilder.name("products")
+        return TopicBuilder.name(KafkaConstants.TOPIC_PRODUCTS)
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+    @Bean
+    public NewTopic orderTopics() {
+        return TopicBuilder.name(KafkaConstants.TOPIC_ORDERS)
                 .partitions(3)
                 .replicas(1)
                 .build();
