c1e0a1fa805da7a5c4cd74247ad22f23b84eb0f4
PriyanthanSeevaratnam <Priyanthan.Seevaratnam@informationtechconsultants.co.uk>
2026-04-25 23:59:19 +0100

    hello

--- backend/catalogue-service/src/main/java/com/itc/catalogueservice/config/KafkaConfig.java
+++ backend/catalogue-service/src/main/java/com/itc/catalogueservice/config/KafkaConfig.java
@@ -7,14 +7,18 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.EnableKafkaRetryTopic;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 @Profile("!test")
 @Configuration
 @EnableKafka
 @EnableKafkaRetryTopic
+@EnableScheduling
 public class KafkaConfig {
 
     @Bean
-    public ThreadPoolTaskScheduler taskScheduler() {
+    public TaskScheduler taskScheduler() {
         ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
         scheduler.setPoolSize(2);
         scheduler.setThreadNamePrefix("kafka-retry-");
