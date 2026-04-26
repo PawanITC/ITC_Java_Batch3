f0fe090673371223703b4048ec2fb13d8c0a513c
PriyanthanSeevaratnam <Priyanthan.Seevaratnam@informationtechconsultants.co.uk>
2026-04-27 00:20:37 +0100

    fix: Isolate TaskScheduler into SchedulerConfig to prevent Spring initialization bug

--- backend/catalogue-service/src/main/java/com/itc/catalogueservice/config/KafkaConfig.java
+++ backend/catalogue-service/src/main/java/com/itc/catalogueservice/config/KafkaConfig.java
@@ -1,26 +1,14 @@
 package com.itc.catalogueservice.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.EnableKafkaRetryTopic;
-import org.springframework.kafka.retrytopic.RetryTopicSchedulerWrapper;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Profile("!test")
 @Configuration
 @EnableKafka
 @EnableKafkaRetryTopic
 public class KafkaConfig {
-
-    @Bean(name = "kafkaRetryTopicSchedulerWrapper")
-    public RetryTopicSchedulerWrapper kafkaRetryTopicSchedulerWrapper() {
-        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
-        scheduler.setPoolSize(2);
-        scheduler.setThreadNamePrefix("kafka-retry-");
-        scheduler.initialize();
-        return new RetryTopicSchedulerWrapper(scheduler);
-    }
 
 }
