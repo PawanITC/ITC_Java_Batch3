b74728d302131af5d5aec2f88d0b775e9eff98e6
PriyanthanSeevaratnam <Priyanthan.Seevaratnam@informationtechconsultants.co.uk>
2026-04-27 00:12:45 +0100

    fix: Explicitly name kafkaRetryTopicSchedulerWrapper bean to fix Spring crash

--- backend/catalogue-service/src/main/java/com/itc/catalogueservice/config/KafkaConfig.java
+++ backend/catalogue-service/src/main/java/com/itc/catalogueservice/config/KafkaConfig.java
@@ -5,25 +5,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.EnableKafkaRetryTopic;
+import org.springframework.kafka.retrytopic.RetryTopicSchedulerWrapper;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-
-import org.springframework.scheduling.TaskScheduler;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Profile("!test")
 @Configuration
 @EnableKafka
 @EnableKafkaRetryTopic
-@EnableScheduling
 public class KafkaConfig {
 
-    @Bean
-    public TaskScheduler taskScheduler() {
+    @Bean(name = "kafkaRetryTopicSchedulerWrapper")
+    public RetryTopicSchedulerWrapper kafkaRetryTopicSchedulerWrapper() {
         ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
         scheduler.setPoolSize(2);
         scheduler.setThreadNamePrefix("kafka-retry-");
         scheduler.initialize();
-        return scheduler;
+        return new RetryTopicSchedulerWrapper(scheduler);
     }
 
 }
