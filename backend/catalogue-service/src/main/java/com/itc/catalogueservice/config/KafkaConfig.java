4ac9542c49c45d692f3231217044654181f88c4f
PriyanthanSeevaratnam <Priyanthan.Seevaratnam@informationtechconsultants.co.uk>
2026-04-27 01:02:42 +0100

    fix: Remove unused Kafka Retry configuration that caused Spring crash

--- backend/catalogue-service/src/main/java/com/itc/catalogueservice/config/KafkaConfig.java
+++ backend/catalogue-service/src/main/java/com/itc/catalogueservice/config/KafkaConfig.java
@@ -3,12 +3,10 @@ package com.itc.catalogueservice.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.annotation.EnableKafkaRetryTopic;
 
 @Profile("!test")
 @Configuration
 @EnableKafka
-@EnableKafkaRetryTopic
 public class KafkaConfig {
 
 }
