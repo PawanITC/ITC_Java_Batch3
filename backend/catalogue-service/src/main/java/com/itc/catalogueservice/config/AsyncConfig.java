e713cc6fd435c39b298d21a96145ad868842b271
PriyanthanSeevaratnam <Priyanthan.Seevaratnam@informationtechconsultants.co.uk>
2026-04-29 19:29:45 +0100

    h

--- backend/catalogue-service/src/main/java/com/itc/catalogueservice/config/AsyncConfig.java
+++ backend/catalogue-service/src/main/java/com/itc/catalogueservice/config/AsyncConfig.java
@@ -12,8 +12,8 @@ public class AsyncConfig {
     @Bean(name = "productExecutor")
     public Executor productExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(240);
-        executor.setMaxPoolSize(340);
+        executor.setCorePoolSize(400);
+        executor.setMaxPoolSize(600);
         executor.setQueueCapacity(700);
         executor.setThreadNamePrefix("product-exec-");
         executor.initialize();
